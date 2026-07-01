cc9ed37ab373590c685dbb7a7dae9b15c3aa3dfd
benchheinrichmendaros-ai <benchheinrichmendaros@gmail.com>
2026-07-01 21:50:56 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,13 @@ import Transport from './pages/Transport';
 import Weather from './pages/Weather';
 import Emergency from './pages/Emergency';
 import BeforeYouGo from './pages/BeforeYouGo';
+import About from './pages/About';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+
+// inside <Routes>:
+<Route path="/about" element={<About />} />
+<Route path="/privacy-policy" element={<PrivacyPolicy />} />
+
 
 // Transport sub-pages
 import Ferry from './pages/transport/Ferry';
@@ -40,10 +47,4 @@ export default function App() {
     </BrowserRouter>
   );
 }
-import About from './pages/About';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-
-// inside <Routes>:
-<Route path="/about" element={<About />} />
-<Route path="/privacy-policy" element={<PrivacyPolicy />} />
 
